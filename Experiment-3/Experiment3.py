5a199e1a8e72ee0d9ebb2ffade94b13d1d52f764
shuaybdawood <shuaybdawood123@gmail.com>
2026-03-11 17:10:08 +0530

    Add files via upload

---
+++ Experiment-3/Experiment3.py
@@ -0,0 +1,29 @@
+a = input("Enter first string: ")
+b = input("Enter second string: ")
+
+print(a.upper())
+print(a.lower())
+print(a.title())
+print(a.swapcase())
+print(a.capitalize())
+
+print(b.upper())
+print(b.lower())
+print(b.title())
+print(b.swapcase())
+print(b.capitalize())
+
+print(a[:5])
+print(b[:5])
+
+print(a.split())
+print(b.split())
+
+print(a.find('h'))
+print(b.find('h'))
+
+print(a.replace('a', 'b'))
+print(b.replace('a', 'b'))
+
+print(len(a))
+print(len(b))
