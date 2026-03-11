b1add4838455a6e644e8c33bf04bc6045a8b8465
shuaybdawood <shuaybdawood123@gmail.com>
2026-03-11 16:59:11 +0530

    Add files via upload

---
+++ Experiment-1/Experiment1.py
@@ -0,0 +1,25 @@
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+
+print("\nEquation assignment Operations:\n")
+print((a+b)**2 == a**2 + 2*(a*b) + b**2)
+print((a-b)**2 == a**2 - 2*(a*b) + b**2)
+print((a+b)*(a+b) == 2*(a**2 - b**2))
+print((a+b)*(a-b) == a**2 - b**2)
+print(a**3 + b**3 == (a+b)*(a**2 - a*b + b**2))
+
+print("\nBitwise Operations:\n")
+print((a|b) == (a^b) + (a&b))
+print(a^(a&b) == (a|b)^b)
+print(b^(a&b) == (a|b)^a)
+print((a&b)^(a|b) == a^b)
+
+print("\nAddition Operations:\n")
+print((a+b) == (a|b) + (a&b))
+print(a+b == (a^b) + 2*(a&b))
+
+print("\nSubtraction Operations:\n")
+print(a-b == (a^(a&b)) - ((a|b)^a))
+print(a-b == ((a|b)^b) - ((a|b)^a))
+print(a-b == (a^(a&b)) - (b^(a&b)))
+print(a-b == ((a|b)^b) - (b^(a&b)))
